884e247047e5bfdfdc7cebccdc926324a93b57b9
user <ojemehprogress@gmail.com>
2021-01-31 11:48:21 +0100

    fixed topNav bug

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,7 @@ const NavBar = () =>{
 			<nav className={styles.topNav, styles.desktopNav}>
 				<ul>
 					<li><Link href="/"><a><img src="images/desktop-logo.png" alt=""/></a></Link></li>
-					<li><Link href="/features"><a>Features</a></Link></li>
+					<li id={styles.secondChild}><Link href="/features"><a>Features</a></Link></li>
 					<li><Link href="/pricing"><a>Pricing</a></Link></li>
 					<li><Link href="/why"><a>Why Gravity</a></Link></li>
 					<li><Link href="/sign-in"><a>Sign In</a></Link></li>
